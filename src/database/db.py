e596101d344b9c9949d6ca43b73d36a247ee74ea
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-10-18 21:28:08 +0300

    changed config.py and connection to database

--- src/database/db.py
+++ src/database/db.py
@@ -1,31 +1,56 @@
+import contextlib
+
 from fastapi import HTTPException, status
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
+    async_sessionmaker,
+    create_async_engine,
+)
 from src.conf.config import settings
 
+
+class DatabaseSessionManager:
+    def __init__(self, url: str):
+        self._engine: AsyncEngine | None = create_async_engine(url)
+        self._session_maker: async_sessionmaker = async_sessionmaker(
+            autoflush=False, autocommit=False, bind=self._engine
+        )
+
+    @contextlib.asynccontextmanager
+    async def session(self):
+        """
+        This method is a context manager that returns the database session.
+        It also handles any exceptions that may occur during the session, and closes
+        the connection when it's done.
+
+        :return: A database session
+        """
+        if self._session_maker is None:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Session is not initialized",
+            )
+        session = self._session_maker()
+        try:
+            yield session
+        except Exception as err:
+            print(err)
+            await session.rollback()
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err)
+            )
+        finally:
+            await session.close()
+
+
 SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+sessionmanager = DatabaseSessionManager(SQLALCHEMY_DATABASE_URL)
 
 
-class Base(DeclarativeBase):
-    pass
-
-
-def get_db():
+async def get_db():
     """
     The get_db function is a context manager that returns the database session.
-    It also handles any exceptions that may occur during the session, and closes
-    the connection when it's done.
-
     :return: A database session
     """
-    db = SessionLocal()
-    try:
-        yield db
-    except SQLAlchemyError as err:
-        db.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err))
-    finally:
-        db.close()
+    async with sessionmanager.session() as session:
+        yield session
