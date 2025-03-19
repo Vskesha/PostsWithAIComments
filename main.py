58889afb994833da20f9779ed325db9558a29e92
vskesha <vskesha@gmail.com>
2025-03-19 02:25:13 +0200

    imports sorted

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from os import path
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from src.conf import messages
-from src.endpoints import answers, auth, comments, posts, users
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -52,7 +50,7 @@ async def healthchecker(db: AsyncSession = Depends(get_db)):
     """
     The healthchecker function is a simple function that checks the health of the database.
     It does this by making a request to the database and checking if it returns any results.
-    If there are no results, then we know something is wrong with our connection to the database.
+    If there are no results, then we know something is wrong with our connection to the database...
 
     :param db: Session: Pass the database session to the function
     :return: A json object with a message
